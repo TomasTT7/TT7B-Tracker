7ce041613ebaf9f76efe88309ab04b7ee75d3fc6
TomasTT7 <tt7tomas@gmail.com>
2018-11-02 01:52:44 +0100

    Add files via upload

--- Accessories/APRSfi_grab.py
+++ Accessories/APRSfi_grab.py
@@ -18,7 +18,7 @@ LIMIT = '25'                        # 5, 25, 50, 100, 300, 500, 1000
 
 """ Source: https://stackoverflow.com/a/12982689 """
 def cleanhtml(raw_html):
-    cleanr = re.compile('<.*?>')
+    cleanr = re.compile(r'<([^>]*)>')
     cleantext = re.sub(cleanr, '', raw_html)
     return cleantext
 
@@ -31,19 +31,74 @@ page = requests.get(url)
 tree = html.fromstring(page.content)
 packets = tree.xpath('//span[@class="raw_line"]')
 
-f = open('APRSfi_grab ' + CALLSIGN + '.txt', 'a')
-fr = open('APRSfi_grab ' + CALLSIGN + '_raw.txt', 'a')
+existing = []
+existing_raw = []
+
+try:
+    with open('APRSfi_grab ' + CALLSIGN + '.txt') as f:
+        existing = f.readlines()
+        f.close
+except:
+    pass
+try:
+    with open('APRSfi_grab ' + CALLSIGN + '_raw.txt') as f:
+        existing_raw = f.readlines()
+        f.close
+except:
+    pass
 
 for packet in packets:
-    processed = htmlparser.unescape(cleanhtml(html.tostring(packet)).replace('&#194;', ''))
-    temp1 = processed.split(' ', 3)
+    temp = html.tostring(packet)
+    temp = temp.replace('&gt;', '>').encode('utf-8')
+    temp = temp.replace('&#194;', '').encode('utf-8')
+    temp = temp.replace('&#160;', ' ').encode('utf-8')
+    tempclean = cleanhtml(temp)
+
+    temp1 = tempclean.split(' ', 3)
     temp2 = temp1[3].split('>', 1)
     temp3 = temp1[3].split(':', 1)
     
-    out = temp1[0] + ' ' + temp1[1] + ' ' + temp1[2].replace(':', '') + ' ' + temp2[0] + ' ' + temp3[1]
-    print(out)
-    f.write(out.encode('utf8') + '\n')
-    fr.write(processed.encode('utf8') + '\n')
+    out = temp1[0] + ' ' + temp1[1] + ' ' + temp1[2].replace(':', '').encode('utf-8') + ' ' + temp2[0] + ' ' + temp3[1]
+    
+    existing.append(out + '\n')
+    existing_raw.append(tempclean + '\n')
 
-f.close()
-fr.close()
+existing_seen = set()
+existing_raw_seen = set()
+
+output = []
+output_raw = []
+
+for pkt in existing:
+    if pkt not in existing_seen:
+        output.append(pkt)
+        existing_seen.add(pkt)
+
+for pkt in existing_raw:
+    if pkt not in existing_raw_seen:
+        output_raw.append(pkt)
+        existing_raw_seen.add(pkt)
+
+output.sort()
+output_raw.sort()
+
+f = open('APRSfi_grab ' + CALLSIGN + '.txt', 'w')
+fr = open('APRSfi_grab ' + CALLSIGN + '_raw.txt', 'w')
+
+for line in output:
+    f.write(line.encode('utf8'))
+    print(line[:-1])
+
+for line in output_raw:
+    fr.write(line.encode('utf8'))
+
+f.close
+fr.close
+
+
+
+
+
+
+
+
